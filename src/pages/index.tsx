ba6f9fd925540f914da3c9d087bde954f4b01cc9
rczerwiec <czerwiecr1999@gmail.com>
2023-02-06 16:31:52 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main style={pageStyles}>
+      xd
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
